a1c05333df34318d475ac806b24c3cc384e56ac4
SHIVANI GARG <shivani@SHIVANIs-MacBook-Pro.local>
2022-04-24 19:35:31 +0530

    Binary tree creation

--- datastructure/binarytree/BinaryTree.java
+++ datastructure/binarytree/BinaryTree.java
@@ -1,9 +1,36 @@
 package datastructure.binarytree;
 
+/*  Simple implementation of binary tree
+ * 
+ */
+
 public class BinaryTree {
+	Node root;
+	
+	// This is default constructor
+	BinaryTree () {
+		root = null;
+	}
+	
+	BinaryTree(Node node) {
+		root = node;
+	}
+	
+	public static BinaryTree doCreate() {
+		System.out.println("Binary Tree Creation");
+		BinaryTree bt = new BinaryTree(new Node(2));
+		bt.root.left = new Node(1);
+		bt.root.right = new Node(8);
+		return bt;
+	}
+
+	public static void traverse(BinaryTree bt) {
+		System.out.println("Traversal of Binary Tree");
+	}
 	
 	public static void main(String[] s) {
-		System.out.println("Binary Tree Creation");
+		BinaryTree bt = doCreate();
+		traverse(bt);
 	}
 
 }
---
+++ datastructure/binarytree/Node.java
@@ -0,0 +1,11 @@
+package datastructure.binarytree;
+
+public class Node {
+	int value;
+	Node right, left;
+	
+	public Node (int value) {
+		this .value = value;
+		right = left = null;
+	}
+}
